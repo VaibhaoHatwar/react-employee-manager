4287a847e15860b66f09a119875e7b4ab0f26551
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-01 18:19:42 +0530

    feat: add AllTasks component to display task stats
    - Created AllTasks.jsx to display task stats (new, active, completed, failed) for each employee
    - Used AuthContext to dynamically retrieve and map employee task data
    - Applied Tailwind styling for structured layout and color-coded task types

--- src/components/common/AllTasks.jsx
+++ src/components/common/AllTasks.jsx
@@ -1,30 +1,30 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
+
 const AllTasks = () => {
+  const authData = useContext(AuthContext);
+
   return (
-    <div className="bg-[#1c1c1c] rounded p-5 mt-5 h-48 overflow-auto">
+    <div className="bg-[#1c1c1c] rounded p-5 mt-5">
       <div className="bg-red-400 mb-2 flex justify-between rounded py-2 px-4">
-        <h2>Vaibhao</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+        <h2 className="text-lg font-medium text-center w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-medium text-center w-1/5">New Task</h3>
+        <h5 className="text-lg font-medium text-center w-1/5"> Active Task</h5>
+        <h5 className="text-lg font-medium text-center w-1/5"> Completed</h5>
+        <h5 className="text-lg font-medium text-center w-1/5"> Failed</h5>
       </div>
-      <div className="bg-green-400 mb-2 flex justify-between rounded py-2 px-4">
-        <h2>Vaibhao</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-yellow-400 mb-2 flex justify-between rounded py-2 px-4">
-        <h2>Vaibhao</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-blue-400 mb-2 flex justify-between rounded py-2 px-4">
-        <h2>Vaibhao</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-purple-400 mb-2 flex justify-between rounded py-2 px-4">
-        <h2>Vaibhao</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+      <div className="overflow-auto">
+        {authData.employees.map((employee) => {
+        return (
+          <div key={employee.id} className="border-2 border-emerald-500 mb-2 flex justify-between rounded py-2 px-4">
+            <h2 className="text-lg font-medium text-center w-1/5">{employee.firstName} {employee.lastName}</h2>
+            <h3 className="text-lg font-medium text-center w-1/5 text-blue-500">{employee.taskCount.newTask}</h3>
+            <h5 className="text-lg font-medium text-center w-1/5 text-yellow-400"> {employee.taskCount.active}</h5>
+            <h5 className="text-lg font-medium text-center w-1/5 text-green-400"> {employee.taskCount.completed}</h5>
+            <h5 className="text-lg font-medium text-center w-1/5 text-red-400"> {employee.taskCount.failed}</h5>
+          </div>
+        );
+      })}
       </div>
     </div>
   );
